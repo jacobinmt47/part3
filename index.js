d6bf4c7e090a6bf5cfbe2a1d095cf8036afbfb2f
jacob l <jacobinmt47@outlook.com>
2019-09-15 14:23:23 -0600

    array persons

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+persons =
+     [
+    {
+      "name": "Arto Hellas",
+      "phoneNumber": "040-123456",
+      "id": 0
+    },
+    {
+      "name": "Ada Lovelace",
+      "phoneNumber": "39-44-5323523",
+      "id": 1
+    },
+    {
+      "name": "Dan Abramov",
+      "phoneNumber": "12-43-234345",
+      "id": 2
+    },
+    {
+      "name": "Mary Poppendieck",
+      "phoneNumber": "39-23-6423122",
+      "id": 3
+    }
+  ]
+
