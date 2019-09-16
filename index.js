0e8a85968f49189f0fe4887bd04e267dded4d05c
jacob l <jacobinmt47@outlook.com>
2019-09-15 20:08:49 -0600

    can add person

--- index.js
+++ index.js
@@ -61,6 +61,21 @@ app.delete('/api/persons/:id',(request,response) =>{
   response.status(204).end()
 })
 
+app.post('/api/persons/',(request,response) =>{
+  const body = request.query
+  console.log(body.name)
+  const person ={
+    name:body.name,
+    phoneNumber:body.phoneNumber,
+    id:Math.round( Math.random()*1000)
+  } 
+  if(!body){
+    return response.status(400).json({error:'query is missing '})
+  }
+  persons = persons.concat(person)
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT,()=>{
     console.log(`listening from port ${PORT}`)
