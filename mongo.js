45fb74a702cb6bbc3790f8df28c0541d919f4782
jacob l <jacobinmt47@outlook.com>
2019-09-18 19:07:03 -0600

    still working on mongo

--- mongo.js
+++ mongo.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 if(process.argv.length<5){
     console.log('not enough arugments ')
     console.log('example   node mongo.js password name 999-999-9999')
+    process.exit(1)
 }
 const password = process.argv[2]
 const name = process.argv[3]
