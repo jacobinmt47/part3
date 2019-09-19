9188809c0afee7959d26a4da7b7e42d12bd5887a
jacob l <jacobinmt47@outlook.com>
2019-09-18 19:05:19 -0600

    mongo.js

---
+++ mongo.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+if(process.argv.length<5){
+    console.log('not enough arugments ')
+    console.log('example   node mongo.js password name 999-999-9999')
+}
+const password = process.argv[2]
+const name = process.argv[3]
+const phonenumber =process.argv[4]
+
