f67eb1acaa3d574f9c862b21538c86c777442a2d
jacob l <jacobinmt47@outlook.com>
2019-09-19 17:40:37 -0600

    working kinda

--- mongo-del.js
+++ mongo-del.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-if(process.argv.length<4){
+if(process.argv.length<3){
     console.log('not enough arugments ')
     console.log('example   node mongo.js password 1')
     process.exit(1)
@@ -8,17 +8,24 @@ const password = process.argv[2]
 const id = process.argv[3]
 const name = process.argv[4]
 const phonenumber = process.argv[5]
-const findall = (phoneSchema)=>{
-  const Record = mongoose.model('record', phoneSchema)
+const logAndClose = (x,i) =>{
+  console.log(x)
+  mongoose.connection.close()
+  process.exit(i)
+}
+const findId = (phoneSchema)=>{
+  console.log('called from findid')
+  const Record = mongoose.model('record',phoneSchema)
   Record.find({'id':id})
-   .then(pn =>{console.log(pn)
-     mongoose.connection.close()
-     process.exit(0)
-   })
-   .catch(error =>{console.log(error)
-    mongoose.connection.close()
-    process.exit(0)
- })
+  .then(x =>{logAndClose(x,0)})
+  .catch(error =>{logAndClose(error,1)})
+}
+const findall = (phoneSchema)=>{
+  console.log('called from findall')
+  const Record = mongoose.model('record', phoneSchema)
+  Record.find({})
+   .then(pn =>{logAndClose(pn,0)})
+   .catch(error =>{logAndClose(error,1)})
 }
 
 const url =`mongodb+srv://jacobinmt47:${password}@cluster0-cekgn.mongodb.net/test?retryWrites=true&w=majority`
@@ -29,5 +36,6 @@ const phoneSchema = new mongoose.Schema({
     name:String,
     phonenumber:String,
   })
- findall(phoneSchema) 
+ if(process.argv.length ===3){findall(phoneSchema)}
+ if(process.argv.length ===4){findId(phoneSchema)} 
  
