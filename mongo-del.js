64d5315bbfdf8f7fc22a8348d7fdc27936b8d408
jacob l <jacobinmt47@outlook.com>
2019-09-20 11:28:08 -0600

    ?

--- mongo-del.js
+++ mongo-del.js
@@ -8,7 +8,12 @@ const password = process.argv[2]
 const id = process.argv[3]
 const name = process.argv[4]
 const phonenumber = process.argv[5]
-const logAndClose = (x,i) =>{
+
+const printNameNumber = (x) =>{
+  console.log(x.name,'  ',x.phonenumber)
+  mongoose.connection.close()
+}
+const logAndCloseError = (x,i) =>{
   console.log(x)
   mongoose.connection.close()
   process.exit(i)
@@ -17,15 +22,27 @@ const findId = (phoneSchema)=>{
   console.log('called from findid')
   const Record = mongoose.model('record',phoneSchema)
   Record.find({'id':id})
-  .then(x =>{logAndClose(x,0)})
-  .catch(error =>{logAndClose(error,1)})
+  .then(pn =>{ pn.forEach(x =>{printNameNumber(x)})})
+  .catch(error =>{logAndCloseError(error,1)})
 }
 const findall = (phoneSchema)=>{
   console.log('called from findall')
   const Record = mongoose.model('record', phoneSchema)
   Record.find({})
-   .then(pn =>{logAndClose(pn,0)})
+   .then(pn =>{ pn.forEach(x =>{printNameNumber(x)})})
    .catch(error =>{logAndClose(error,1)})
+}
+
+const addPerson = (phoneSchema) =>{
+  console.log('called from addPersons')
+  const Record = mongoose.model('record', phoneSchema)
+  rec = new Record({
+    id:id,
+    name:name,
+    phonenumber:phonenumber
+  })
+  rec.save().then(x =>{printNameNumber(x)})
+  .catch(x =>{logAndClose(x,1)})
 }
 
 const url =`mongodb+srv://jacobinmt47:${password}@cluster0-cekgn.mongodb.net/test?retryWrites=true&w=majority`
@@ -36,6 +53,7 @@ const phoneSchema = new mongoose.Schema({
     name:String,
     phonenumber:String,
   })
- if(process.argv.length ===3){findall(phoneSchema)}
- if(process.argv.length ===4){findId(phoneSchema)} 
+ if(process.argv.length === 3){findall(phoneSchema)}
+ if(process.argv.length === 4){findId(phoneSchema)} 
+ if(process.argv.length === 6){addPerson(phoneSchema)}
  
