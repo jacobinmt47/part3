e7e734073c13c9f298fc7dc026f585e6f29462c0
jacob l <jacobinmt47@outlook.com>
2019-09-23 14:40:00 -0600

    commit 1

--- mg.js
+++ mg.js
@@ -4,7 +4,8 @@ const id = process.argv[2]
 const name = process.argv[3]
 const phonenumber = process.argv[4]
 
-const ret = (x) =>{x.forEach(y=>{ console.log(y.name,'  ',y.phonenumber)})}
+const ret = (x) =>{x.forEach(y=>{ console.log(y)})}
+m.update(id,name,phonenumber)
 m.findAll(ret)
 //m.deleteId(id)
 //m.addPerson(id,name,phonenumber)
