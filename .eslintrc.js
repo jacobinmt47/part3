97b57e86fe9449fa2160a7f660b5778794520c45
jacob l <jacobinmt47@outlook.com>
2019-10-05 16:28:36 -0600

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "eqeqeq":'error',
+    'no-trailing-spaces':'error',
+    "object-curly-spacing": [
+      "error", "always"
+  ],
+  "arrow-spacing": [
+      "error", { "before": true, "after": true }
+  ],
+  'no-console':0
+  }
 };
